d338fc43c0d7b57c158ab71224b3a513de03bb39
lnpad007 <50189883+lnpad007@users.noreply.github.com>
2021-05-25 17:18:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,24 +40,6 @@ module "vpc" {
   tags = {}
 }
 
-//resource "aws_security_group" "k8s_common_http" {
-//  name   = "${local.environment}_k8s_common_http"
-//  vpc_id = module.vpc.id
-//  ingress {
-//    from_port   = 80
-//    protocol    = "tcp"
-//    to_port     = 80
-//    cidr_blocks = [local.ingress_ips]
-//  }
-//
-//  ingress {
-//    from_port   = 443
-//    protocol    = "tcp"
-//    to_port     = 443
-//    cidr_blocks = [local.ingress_ips]
-//  }
-//}
-
 resource "aws_security_group" "ssh_ingress" {
   vpc_id = module.vpc.id
 
